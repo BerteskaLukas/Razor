fda9c5dc6f513d017700640c1ffa3e8c768c1e7b
Mukuruku <berteskalukas@gmail.com>
2021-04-16 07:04:34 +0300

    Frontend validation

--- Razor/Models/Client.cs
+++ Razor/Models/Client.cs
@@ -1,27 +1,28 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Razor.Models
 {
         public class Client
         {
-            //[Required]
+            [Required]
             public string FirstName { get; set; }
-            //[Required]
+            [Required]
             public string LastName { get; set; }
-            //[Required]
+            [Required]
             public string CompanyName { get; set; }
-            //[Required]
-            //[EmailAddress]
+            [Required]
+            [EmailAddress]
             //[Remote("VerifyEmail", "Client")]
             public string Email { get; set; }
-            //[Required]
+            [Required]
             public string Phone { get; set; }
-            //[Required]
+            [Required]
             public DateTime? ContactDate { get; set; }
-            //[Required]
+            [Required]
             public string ClientType { get; set; }
-            //[Required]
-            //[MaxLength(25)]
+            [Required]
+            [MaxLength(25)]
             public string Notes { get; set; }
             public Address BillingAddress { get; set; }
             public Address MailingAddress { get; set; }
